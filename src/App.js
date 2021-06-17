16dcd151585ecf2466d22f7fadddb29e8e8a7e02
vibhu sharma <vibhusharma8447@gmail.com>
2021-06-18 00:41:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Review from './Review';
+import './App.css';
+function App() {
+  return (
+<main > 
+<   div className="title">
+      <h2> Our Reviews</h2>
+    </div>
+  <section className="container">
+
+    <Review></Review>
+  </section>
+
+
+</main>
+  );
+}
+
+export default App;
---
+++ src/Review.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import {FaAngleLeft} from 'react-icons/all';
+import {FaAngleRight} from 'react-icons/all';
+import {FaQuoteRight} from 'react-icons/all';
+import people from "./Reviews";
+import './App.css';
+
+ function Review()
+ {
+    const [index,setindex]=useState(0);
+    const {name ,job,image,text}=people[index];
+
+    return(
+        <article className="review">
+            <div className="img-container">
+                <img src={image} alt={name} className="person-img"/>
+                <span>
+                        <FaQuoteRight/>
+                </span>
+                
+            </div>
+            <h4 className="author">{name}</h4>
+            <p className="job" >{job}</p>
+            <p className="text">{text}</p>
+             <FaAngleRight className="forward_arrow" onClick={()=>{
+                 const forw=index+1;
+                 if(forw===4)
+                 setindex(0);
+                 else
+                 setindex(forw);
+             }}/>
+             <FaAngleLeft className="backward_arraw" onClick={()=>{
+                 const bacw=index-1;
+                 if(bacw==-1)
+                 setindex(3);
+                 else
+                 setindex(bacw);
+             }}/>
+        </article>
+    );
+ }
+ export default Review;
